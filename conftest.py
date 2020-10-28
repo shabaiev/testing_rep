cd5ca4bb6aa25dd7f2d9ef6d1a75043b17147702
shabaiev <ilyashabaev2209@gmail.com>
2020-10-28 11:01:18 -0500

    deleted decorator

--- conftest.py
+++ conftest.py
@@ -1,27 +1,31 @@
 import pytest
 import requests
+
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption("--browser", default="chrome")
+    parser.addoption("--browser", default="safari")
+    parser.addoption("--driver_folder", default="/Users/Ilya/Documents/drivers")
 
 
-@pytest.fixture(scope="session", params=["chrome", "firefox", "opera", "safari"])
+@pytest.fixture(scope="session")
 def browser(request):
-    driver_path = "/Users/Ilya/Documents/drivers"
-    browser = request.param
+    browser = request.config.getoption("chrome")
+    folder = request.config.getoption("driver_folder")
+
     if browser == "firefox":
-        driver = webdriver.Firefox(executable_path=f"{driver_path}/geckodriver")
+        driver = webdriver.Firefox(executable_path=f"{folder}/geckodriver")
     elif browser == "opera":
-        driver = webdriver.Opera(executable_path=f"{driver_path}/operadriver")
+        driver = webdriver.Opera(executable_path=f"{folder}/operadriver")
     elif browser == "safari":
         driver = webdriver.Safari()
     else:
-        driver = webdriver.Chrome()
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        driver = webdriver.Chrome(options=chrome_options)
+
     request.addfinalizer(driver.close)
     driver.maximize_window()
     return driver
-
-
-
